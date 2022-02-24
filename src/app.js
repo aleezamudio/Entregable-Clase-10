cd6ae426ab8d1dce11e0602d448db9758dafd5c9
Zamudio Alejandro <ale-zamudio2011@hotmail.com>
2022-02-24 01:15:43 -0300

    entregable clase 12

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {Server}=require('socket.io');
+const PetManager = require('./Manager/PetManager.js');
+
+const PORT= 8080;
+const app = express();
+const server = app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+const io = new Server(server);
+app.use(express.static(__dirname+'/public'))
+
+//services
+const petService = new PetManager();
+io.on('connection',async socket=>{
+    console.log("cliente conectado");
+    let pets = await petService.getAll();
+    io.emit('petLog',pets)
+    socket.on('sendPet', async data=>{
+        await petService.add(data);
+        let pets = await petService.getAll();
+        io.emit('petLog',pets)
+    })
+})
